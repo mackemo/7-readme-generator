a285d2d7d889645519e5ed91e27791bcc0b38aff
mackemo <mackenzielmoore14@gmail.com>
2024-05-08 12:11:09 -0400

    entered questions for each section of README in questions array

--- index.js
+++ index.js
@@ -3,7 +3,38 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description for your project.', 
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?', 
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information.', 
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute?', 
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter how to test your project.', 
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
