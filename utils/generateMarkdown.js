260a319054ea8f19d27550be59861979b6ef94cd
mackemo <mackenzielmoore14@gmail.com>
2024-05-15 10:55:12 -0400

    render license section, i added a statment to be added based on the license chosen

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,7 +7,7 @@ function renderLicenseBadge(license) {
 
     case 'Apache License 2.0':
       licenseBadge =
-      '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+      '[![Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
       break;
 
 
@@ -81,6 +81,8 @@ function renderLicenseBadge(license) {
       licenseBadge = "";
   }
 
+  licenseBadge += `(${renderLicenseLink(license)})`
+
   return licenseBadge;
 }
 
@@ -93,73 +95,73 @@ function renderLicenseLink(license) {
 
     case 'Apache License 2.0':
       licenseLink =
-      '(https://opensource.org/licenses/Apache-2.0)';
+      'https://opensource.org/licenses/Apache-2.0';
       break;
 
 
     case 'GNU':
       licenseLink =
-      '(https://www.gnu.org/licenses/gpl-3.0)';
+      'https://www.gnu.org/licenses/gpl-3.0';
       break;
 
 
     case 'MIT':
       licenseLink =
-      '(https://opensource.org/licenses/MIT)';
+      'https://opensource.org/licenses/MIT';
       break;
 
 
     case 'BSD 2-clause':
       licenseLink =
-      '(https://opensource.org/licenses/BSD-2-Clause)';
+      'https://opensource.org/licenses/BSD-2-Clause';
       break;
 
 
     case 'BSD 3-clause':
       licenseLink =
-      '(https://opensource.org/licenses/BSD-3-Clause)';
+      'https://opensource.org/licenses/BSD-3-Clause';
       break;
 
 
     case 'Boost':
       licenseLink = 
-      '(https://www.boost.org/LICENSE_1_0.txt)';
+      'https://www.boost.org/LICENSE_1_0.txt';
       break;
 
 
     case 'Creative Commons':
       licenseLink =
-      '(http://creativecommons.org/publicdomain/zero/1.0/)';
+      'http://creativecommons.org/publicdomain/zero/1.0/';
       break;
 
 
     case 'Eclipse':
       licenseLink =
-      '(https://opensource.org/licenses/EPL-1.0)';
+      'https://opensource.org/licenses/EPL-1.0';
       break;
 
 
     case 'GNU v3':
       licenseLink =
-      '(https://www.gnu.org/licenses/agpl-3.0)';
+      'https://www.gnu.org/licenses/agpl-3.0';
       break;
 
 
     case 'GNU v2':
       licenseLink = 
-      '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+      'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
       break;
 
 
     case 'Mozilla':
       licenseLink =
-      '(https://opensource.org/licenses/MPL-2.0)';
+      'https://opensource.org/licenses/MPL-2.0';
       break;
 
 
     case 'The Unlicense':
       licenseLink = 
-      '(http://unlicense.org/)';
+      'http://unlicense.org/';
       break;
 
 
@@ -172,11 +174,18 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `This repository is using the [${license}](${renderLicenseLink(license)}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
 
 `;
 }
